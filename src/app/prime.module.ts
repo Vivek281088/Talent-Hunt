a8c8ff38e01aa2ba269b6abfcfcb9abec17b4425
Kabilanmph <kabilan.c@mphasis.com>
2023-08-31 11:38:21 +0530

    Changes for dashboad component

--- src/app/prime.module.ts
+++ src/app/prime.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { DropdownModule } from 'primeng/dropdown';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @NgModule({
   imports: [
         MultiSelectModule,
         DropdownModule,
-        ButtonModule
+        ButtonModule,
+        CardModule,
+        CheckboxModule
     ],
     exports: [
         MultiSelectModule,
         DropdownModule,
-        ButtonModule
+        ButtonModule,
+        CardModule,
+        CheckboxModule
 
     ]
 })
--- src/app/services/skillsdropdown.service.ts
+++ src/app/services/skillsdropdown.service.ts
@@ -29,3 +29,4 @@ getskillsList(): Observable<any>{
 }
 
 }
+
