d6c38be5e604cf2a74860ff6b9b321f8b2cad7b1
Baranimph <baranidharan.s@mphasis.com>
2023-09-28 17:14:38 +0530

    login authentication done

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 
 import { Router } from '@angular/router';
 
+import { LoginService } from 'src/app/services/login.service';
+
  
 
 @Component({
@@ -21,14 +23,49 @@ import { Router } from '@angular/router';
 
 export class LoginComponent {
 
-  constructor(private router: Router) {}
+  name!:string;
+  password!:string;
+  nameinvalid!:string;
+  passwordinvalid!:string;
+  constructor(private router: Router,
+    private loginservice:LoginService) {}
 
  
 
   Sign() {
+    // if(this.name ==null){
+    //   this.nameinvalid="Please enter the name";
+    //   //alert("please enter the name")
+    // }
+    // else if(this.password==null){
+    //   alert("please enter password")
+    // }
+    // else{
+    
+    
+    this.loginservice.postlogincredentials(this.name,this.password).subscribe((data)=>{
+      console.log("authenticate",data);
+     
+      if(data.status==200){
+        localStorage.setItem("localuserdata",JSON.stringify(data))
+        if(data.role=="manager"){
+          this.router.navigate(['dashboard'])
+        }
+        else if(data.role=="user"){
+          this.router.navigate(['signup'])
+        }
+      }
+      else{
+        alert(data.message);
+      }
 
-    this.router.navigate(['dashboard']);
+    })
+  }
+
+
+    
+    // this.router.navigate(['dashboard']);
 
   }
 
-}
+// }
