3ea9f66f3a3df8f90d2c66d62f6ec7984e5a52da
kabilanmph <kabilan.c@mphasis.com>
2023-12-06 17:43:10 +0530

    Sidebar

--- src/app/modules/sidenavbar/sidenavbar.component.ts
+++ src/app/modules/sidenavbar/sidenavbar.component.ts
@@ -7,8 +7,26 @@ import * as $ from 'jquery';
   styleUrls: ['./sidenavbar.component.scss'],
 })
 export class SidenavbarComponent {
-  
+  assessmentShow: boolean = false;
+  userShow: boolean = false;
+  questionBankShow: boolean = false;
+  systemShow: boolean = false;
+  isSideNavCollapsed: boolean = false;
 
+  toggleSideNav() {
+    this.isSideNavCollapsed = !this.isSideNavCollapsed;
+  }
 
-  
+  changeStyle1() {
+    this.assessmentShow = !this.assessmentShow;
+  }
+  changeStyle2() {
+    this.userShow = !this.userShow;
+  }
+  changeStyle3() {
+    this.questionBankShow = !this.questionBankShow;
+  }
+  changeStyle4() {
+    this.systemShow = !this.systemShow;
+  }
 }
