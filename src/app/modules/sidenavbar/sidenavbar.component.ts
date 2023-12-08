b592f5d17f3edacd8f67e9907eda9f516b7e6e56
Kabilanmph <kabilan.c@mphasis.com>
2023-12-08 15:15:27 +0530

    8/12/23 changes

--- src/app/modules/sidenavbar/sidenavbar.component.ts
+++ src/app/modules/sidenavbar/sidenavbar.component.ts
@@ -11,9 +11,62 @@ export class SidenavbarComponent {
   questionBankShow: boolean = false;
   systemShow: boolean = false;
   isSideNavCollapsed: boolean = false;
+  activeSection: string = 'home';
+  activeSublist: string = '';
+
+  ngOnInit(): void {
+    this.changeStyle('home');
+  }
 
   toggleSideNav() {
     this.isSideNavCollapsed = !this.isSideNavCollapsed;
+  }
+
+  changeStyle(section: string) {
+    const sections = ['home', 'assessment', 'user', 'question_bank', 'system'];
+
+    //remove active class for all
+    sections.forEach((sec) => {
+      const element = document.getElementById(sec);
+      if (element) {
+        element.classList.remove('active');
+      }
+
+      //add active
+      const activeElement = document.getElementById(section);
+      if (activeElement) {
+        activeElement.classList.add('active');
+      }
+
+      //updating active section
+      this.activeSection = section;
+    });
+  }
+
+  changeSublist(sublist: string) {
+    const sublists = [
+      'manageSchedule',
+      'manageAssessment',
+      'manageManagers',
+      'manageCandidates',
+      'manageSkills',
+      'manageQuestions',
+      'manageProfile',
+      'configuration',
+    ];
+    sublists.forEach((sub) => {
+      const sublistElement = document.getElementById(sub);
+      if (sublistElement) {
+        sublistElement.classList.remove('active');
+      }
+    });
+
+    const activeSublistElement = document.getElementById(sublist);
+    if (activeSublistElement) {
+      activeSublistElement.classList.add('active');
+    }
+
+    this.activeSublist = sublist;
   }
 
   changeStyle1() {
