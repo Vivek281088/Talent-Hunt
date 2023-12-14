0a1b917f27a5601bfbbf8104f4a6aa1f1940b22b
kabilanmph <kabilan.c@mphasis.com>
2023-12-14 23:24:20 +0530

    new schedule created

--- src/app/modules/new-schedule/new-schedule.component.ts
+++ src/app/modules/new-schedule/new-schedule.component.ts
@@ -8,6 +8,8 @@ import { MenuItem } from 'primeng/api';
 })
 export class NewScheduleComponent {
   items: MenuItem[] | undefined;
+  tabs: { title: string, content: string }[] = [];
+  Tdata !: any;
 
   constructor() {}
   ngOnInit() {
@@ -16,5 +18,30 @@ export class NewScheduleComponent {
       { label: 'Assessment', routerLink: 'dashboard' },
       { label: 'New Schedule', routerLink: 'new-schedule' },
     ];
+
+    this.tabs = [
+      { title: 'JavaScript', content: 'Tab 1 Content' },
+      { title: 'NodeJS', content: 'Tab 2 Content' },
+      { title: 'React', content: 'Tab 3 Content' },
+      { title: 'MongoDB', content: 'Tab 3 Content' }
+  ];
+
+  this.Tdata={
+    
+    "question": "Which of the following is not a functional interface in Java 8?",
+    "questionType": "Radio",
+    "options": [
+      "Consumer",
+      "Supplier",
+      "Runnable",
+      "Comparator"
+    ],
+    "skills": "Java-8",
+    "Difficulty_Level": "E",
+    "answer": [
+      "Comparator"
+    ]
+  }
+
   }
 }
