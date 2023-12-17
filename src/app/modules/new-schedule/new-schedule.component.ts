362c8db770fb9b759bcecb3244474df2eff79e6f
kabilanmph <kabilan.c@mphasis.com>
2023-12-17 20:55:32 +0530

    weekend commit

--- src/app/modules/new-schedule/new-schedule.component.ts
+++ src/app/modules/new-schedule/new-schedule.component.ts
@@ -10,6 +10,7 @@ export class NewScheduleComponent {
   items: MenuItem[] | undefined;
   tabs: { title: string; content: any }[] = [];
   Tdata!: any;
+  selectedQuestion!: any;
 
   constructor() {}
   ngOnInit() {
@@ -18,6 +19,97 @@ export class NewScheduleComponent {
       { label: 'Assessment', routerLink: 'dashboard' },
       { label: 'New Schedule', routerLink: 'new-schedule' },
     ];
+    this.Tdata = [{
+      id : 1,
+      question: "Which of the following is not a functional interface in Java 8?",
+      questionType: "Single Answer",
+      options: [
+        "Consumer",
+        "Supplier",
+        "Runnable",
+        "Comparator"
+      ],
+      skills: "Java-8",
+      Difficulty_Level: "Intermediate",
+      answer: [
+        "Comparator"
+      ]
+    },{
+      id : 2,
+      question: "Which is the new method introduced in the String class in Java 8?",
+      questionType: "Multi Answer",
+      options: [
+        "Consumer",
+        "Supplier",
+        "Runnable",
+        "Comparator"
+      ],
+      skills: "Java-8",
+      Difficulty_Level: "Expert",
+      answer: [
+        "Comparator"
+      ]
+    },{
+      id : 3,
+      question: "Which of the following is a valid lambda expression in Java 8?",
+      questionType: "Multi Answer",
+      options: [
+        "Consumer",
+        "Supplier",
+        "Runnable",
+        "Comparator"
+      ],
+      skills: "Java-8",
+      Difficulty_Level: "Beginner",
+      answer: [
+        "Comparator"
+      ]
+    },{
+      id : 4,
+      question: "Which of the following is not a functional interface in Java 8?",
+      questionType: "Single Answer",
+      options: [
+        "Consumer",
+        "Supplier",
+        "Runnable",
+        "Comparator"
+      ],
+      skills: "Java-8",
+      Difficulty_Level: "Expert",
+      answer: [
+        "Comparator"
+      ]
+    },{
+      id : 5,
+      question: "What is the output of the program?List<String> names = Arrays.asList('ABC', 'CAB', 'BCA')",
+      questionType: "Multi Answer",
+      options: [
+        "Consumer",
+        "Supplier",
+        "Runnable",
+        "Comparator"
+      ],
+      skills: "Java-8",
+      Difficulty_Level: "Intermediate",
+      answer: [
+        "Comparator"
+      ]
+    },{
+      id : 6,
+      question: "What is Java?",
+      questionType: "Single Answer",
+      options: [
+        "Consumer",
+        "Supplier",
+        "Runnable",
+        "Comparator"
+      ],
+      skills: "Java-8",
+      Difficulty_Level: "Beginner",
+      answer: [
+        "Comparator"
+      ]
+    }]
 
     this.tabs = [
       { title: 'JavaScript', content: this.Tdata },
@@ -26,22 +118,7 @@ export class NewScheduleComponent {
       { title: 'MongoDB', content: this.Tdata },
     ];
 
-    this.Tdata = [{
-
-      question: "Which of the following is not a functional interface in Java 8?",
-      questionType: "Radio",
-      options: [
-        "Consumer",
-        "Supplier",
-        "Runnable",
-        "Comparator"
-      ],
-      skills: "Java-8",
-      Difficulty_Level: "E",
-      answer: [
-        "Comparator"
-      ]
-    }]
+   
   }
 
   // createTdataInstance() {
