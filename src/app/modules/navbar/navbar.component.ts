2060a082e46d29891a540fd788f25a8d81d6610b
Kabilanmph <kabilan.c@mphasis.com>
2023-12-20 15:54:22 +0530

    manager page changes

--- src/app/modules/navbar/navbar.component.ts
+++ src/app/modules/navbar/navbar.component.ts
@@ -2,104 +2,64 @@ import { Component } from '@angular/core';
 import { AuthService } from 'src/app/Guard/auth.service';
 import { ManagernameService } from 'src/app/services/managername.service';
 import { CandidateAssessmentService } from 'src/app/services/candidate-assessment.service';
-
+ 
+ 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-  isDropdownOpen: boolean = false;
+ 
+ 
+  overlayVisible: boolean = false;
   candidateName: string = '';
   candidateList: any[] = [];
-
   showCandidateEmail!: string;
-
   finalizedEmail!: string;
   userName!: string | null;
   userEmail!: string;
   userPhone!: number;
   name: boolean = false;
-
-  isEditProfile: boolean = false;
-
+  modalVisible: boolean = false;
+ 
+ 
   finalizedManagerEmail!: string;
   visible: boolean = false;
   tempUserName!: string | null;
   id!: number;
-
+ 
   constructor(
     private authservice: AuthService,
     private managernameService: ManagernameService,
     private candidateService: CandidateAssessmentService
   ) {}
   ngOnInit(): void {
-    // this.finalizedEmail =
-    //   this.managernameService.getCandidateAssessment_Email();
-    // this.finalizedManagerEmail = this.managernameService.getManagerName_Email();
-    //--------------------------------------
+ 
     this.authUserOrManager();
-
-    // this.finalizedManagerEmail = localStorage.getItem('managerEmail')!;
-    // this.finalizedEmail = localStorage.getItem('Candidateemail')!;
-
-    // const a = localStorage.getItem('userrole');
-
-    // //  this.finalizedEmail= this.managernameService.getCandidateAssessment_Email();
-    // if (a == 'manager') {
-    //   this.managernameService
-    //     .getManagerdata_by_Email(this.finalizedManagerEmail)
-    //     .subscribe((response) => {
-    //       console.log('Navbar-res', response);
-    //       this.tempUserName = response[0].Managername;
-    //       this.userName = response[0].Managername;
-    //       this.id = response[0].id;
-    //       this.userEmail = response[0].candidateEmail;
-    //       this.userPhone = response[0].phoneNumber;
-
-    //       this.managernameService.setManagerName_Email(this.userEmail);
-    //       this.name = true;
-
-    //       // this.candidateName = response[0].candidateName;
-    //     });
-    // } else {
-    //   this.candidateService
-    //     .getCandidatedata_by_Email(this.finalizedEmail)
-    //     .subscribe((response) => {
-    //       this.name = false;
-    //       this.candidateList = response;
-    //       this.tempUserName = response[0].candidateName;
-    //       this.userName = response[0].candidateName;
-    //       this.id = response[0].id;
-    //       this.userEmail = response[0].candidateEmail;
-    //       this.userPhone = response[0].candidatePhone;
-    //       console.log('candidateName', this.candidateName);
-    //     });
-    // }
+ 
+ 
   }
   authUserOrManager() {
     this.finalizedManagerEmail = localStorage.getItem('managerEmail')!;
     this.finalizedEmail = localStorage.getItem('Candidateemail')!;
-
+ 
     const a = localStorage.getItem('userrole');
-
-    //  this.finalizedEmail= this.managernameService.getCandidateAssessment_Email();
+ 
     if (a == 'manager') {
       this.managernameService
         .getManagerdata_by_Email(this.finalizedManagerEmail)
         .subscribe((response) => {
           console.log('Navbar-res', response);
-          this.tempUserName =
-            response[0].Firstname + ' ' + response[0].Lastname;
-          this.userName = response[0].Firstname + ' ' + response[0].Lastname;
+          this.tempUserName = response[0].Firstname + ' '+ response[0].Lastname;
+          this.userName = response[0].Firstname + ' '+ response[0].Lastname;
           this.id = response[0].id;
           this.userEmail = response[0].candidateEmail;
           this.userPhone = response[0].phoneNumber;
-
+ 
           this.managernameService.setManagerName_Email(this.userEmail);
           this.name = true;
-
-          // this.candidateName = response[0].candidateName;
+ 
         });
     } else {
       this.candidateService
@@ -116,30 +76,27 @@ export class NavbarComponent {
         });
     }
   }
-
+ 
   authUserOrManager1() {
     this.finalizedManagerEmail = localStorage.getItem('managerEmail')!;
     this.finalizedEmail = localStorage.getItem('Candidateemail')!;
-
+ 
     const a = localStorage.getItem('userrole');
-
-    //  this.finalizedEmail= this.managernameService.getCandidateAssessment_Email();
+ 
     if (a == 'manager') {
       this.managernameService
         .getManagerdata_by_Email(this.finalizedManagerEmail)
         .subscribe((response) => {
           console.log('Navbar-res', response);
-          this.tempUserName =
-            response[0].Firstname + ' ' + response[0].Lastname;
-          this.userName = response[0].Firstname + ' ' + response[0].Lastname;
+          this.tempUserName = response[0].Firstname + ' '+ response[0].Lastname;
+          this.userName = response[0].Firstname + ' '+ response[0].Lastname;
           this.id = response[0].id;
           this.userEmail = response[0].candidateEmail;
           this.userPhone = response[0].phoneNumber;
-
+ 
           this.managernameService.setManagerName_Email(this.userEmail);
           this.name = true;
-
-          // this.candidateName = response[0].candidateName;
+ 
         });
     } else {
       this.candidateService
@@ -157,81 +114,26 @@ export class NavbarComponent {
     }
     this.refreshPage();
   }
-
+ 
   refreshPage() {
-    // Reload the current page
     window.location.reload();
   }
-
-  toggleDropdown() {
-    this.isDropdownOpen = !this.isDropdownOpen;
-  }
-
-  redirectToProfile() {
-    this.visible = true;
-  }
-
-  logout() {
-    this.authservice.logout();
-    localStorage.clear();
-    // Redirect to the login page
-    // Example: this.router.navigate(['/login']);
-  }
-  closedialog() {
-    this.isEditProfile = false;
-  }
-
-  updateDetails() {
-    if (this.isEditProfile) {
-      const a = localStorage.getItem('userrole');
-      if (a == 'manager') {
-        const data = {
-          Managername: this.tempUserName || this.userName,
-          candidateEmail: this.userEmail,
-          phoneNumber: this.userPhone,
-          id: this.id,
-        };
-        console.log('profile data', data);
-
-        this.candidateService
-          .updateManagerProfile(data)
-          .subscribe((response) => {
-            console.log('Profile updated successfully', data);
-          });
-      } else {
-        //For candidate
-        const data = {
-          candidateName: this.tempUserName || this.userName,
-          candidateEmail: this.userEmail,
-          candidatePhone: this.userPhone,
-        };
-
-        this.candidateService
-          .updateCandidateProfile(data)
-          .subscribe((response) => {
-            console.log('Profile updated successfully', data);
-          });
-      }
-
-      // this.authUserOrManager1();
-      // this.refreshPage();
-      this.isEditProfile = false;
-    }
-    this.authUserOrManager1();
-    // this.refreshPage();
-  }
-
-  toggleEditProfile() {
-    this.tempUserName = this.userName;
-    this.isEditProfile = !this.isEditProfile;
-  }
+ 
   getInitials(name: string | null): string {
     if (!name) {
       return '';
     }
-
+ 
     const names = name.split(' ');
     const initials = names.map((n) => n.charAt(0)).join('');
     return initials.toUpperCase();
   }
+ 
+  toggle() {
+    this.overlayVisible = !this.overlayVisible;
 }
+ 
+logout(){
+  this.authservice.logout();
+}
+ }
--- src/app/modules/new-schedule/new-schedule.component.ts
+++ src/app/modules/new-schedule/new-schedule.component.ts
@@ -19,97 +19,67 @@ export class NewScheduleComponent {
       { label: 'Assessment', routerLink: 'dashboard' },
       { label: 'New Schedule', routerLink: 'new-schedule' },
     ];
-    this.Tdata = [{
-      id : 1,
-      question: "Which of the following is not a functional interface in Java 8?",
-      questionType: "Single Answer",
-      options: [
-        "Consumer",
-        "Supplier",
-        "Runnable",
-        "Comparator"
-      ],
-      skills: "Java-8",
-      Difficulty_Level: "Intermediate",
-      answer: [
-        "Comparator"
-      ]
-    },{
-      id : 2,
-      question: "Which is the new method introduced in the String class in Java 8?",
-      questionType: "Multi Answer",
-      options: [
-        "Consumer",
-        "Supplier",
-        "Runnable",
-        "Comparator"
-      ],
-      skills: "Java-8",
-      Difficulty_Level: "Expert",
-      answer: [
-        "Comparator"
-      ]
-    },{
-      id : 3,
-      question: "Which of the following is a valid lambda expression in Java 8?",
-      questionType: "Multi Answer",
-      options: [
-        "Consumer",
-        "Supplier",
-        "Runnable",
-        "Comparator"
-      ],
-      skills: "Java-8",
-      Difficulty_Level: "Beginner",
-      answer: [
-        "Comparator"
-      ]
-    },{
-      id : 4,
-      question: "Which of the following is not a functional interface in Java 8?",
-      questionType: "Single Answer",
-      options: [
-        "Consumer",
-        "Supplier",
-        "Runnable",
-        "Comparator"
-      ],
-      skills: "Java-8",
-      Difficulty_Level: "Expert",
-      answer: [
-        "Comparator"
-      ]
-    },{
-      id : 5,
-      question: "What is the output of the program?List<String> names = Arrays.asList('ABC', 'CAB', 'BCA')",
-      questionType: "Multi Answer",
-      options: [
-        "Consumer",
-        "Supplier",
-        "Runnable",
-        "Comparator"
-      ],
-      skills: "Java-8",
-      Difficulty_Level: "Intermediate",
-      answer: [
-        "Comparator"
-      ]
-    },{
-      id : 6,
-      question: "What is Java?",
-      questionType: "Single Answer",
-      options: [
-        "Consumer",
-        "Supplier",
-        "Runnable",
-        "Comparator"
-      ],
-      skills: "Java-8",
-      Difficulty_Level: "Beginner",
-      answer: [
-        "Comparator"
-      ]
-    }]
+    this.Tdata = [
+      {
+        id: 1,
+        question:
+          'Which of the following is not a functional interface in Java 8?',
+        questionType: 'Single Answer',
+        options: ['Consumer', 'Supplier', 'Runnable', 'Comparator'],
+        skills: 'Java-8',
+        Difficulty_Level: 'Intermediate',
+        answer: ['Comparator'],
+      },
+      {
+        id: 2,
+        question:
+          'Which is the new method introduced in the String class in Java 8?',
+        questionType: 'Multi Answer',
+        options: ['Consumer', 'Supplier', 'Runnable', 'Comparator'],
+        skills: 'Java-8',
+        Difficulty_Level: 'Expert',
+        answer: ['Comparator'],
+      },
+      {
+        id: 3,
+        question:
+          'Which of the following is a valid lambda expression in Java 8?',
+        questionType: 'Multi Answer',
+        options: ['Consumer', 'Supplier', 'Runnable', 'Comparator'],
+        skills: 'Java-8',
+        Difficulty_Level: 'Beginner',
+        answer: ['Comparator'],
+      },
+      {
+        id: 4,
+        question:
+          'Which of the following is not a functional interface in Java 8?',
+        questionType: 'Single Answer',
+        options: ['Consumer', 'Supplier', 'Runnable', 'Comparator'],
+        skills: 'Java-8',
+        Difficulty_Level: 'Expert',
+        answer: ['Comparator'],
+      },
+      {
+        id: 5,
+        question:
+          "What is the output of the program?List<String> names = Arrays.asList('ABC', 'CAB', 'BCA')",
+        questionType: 'Multi Answer',
+        options: ['Consumer', 'Supplier', 'Runnable', 'Comparator'],
+        skills: 'Java-8',
+        Difficulty_Level: 'Intermediate',
+        answer: ['Comparator'],
+      },
+      {
+        id: 6,
+        question: 'What is Java?',
+        questionType: 'Single Answer',
+        options: ['Consumer', 'Supplier', 'Runnable', 'Comparator'],
+        skills: 'Java-8',
+        Difficulty_Level: 'Beginner',
+        answer: ['Comparator'],
+      },
+    ];
 
     this.tabs = [
       { title: 'JavaScript', content: this.Tdata },
@@ -117,19 +87,22 @@ export class NewScheduleComponent {
       { title: 'React', content: this.Tdata },
       { title: 'MongoDB', content: this.Tdata },
     ];
-
-   
   }
 
-  // createTdataInstance() {
-  //   return {
-  //     question:
-  //       'Which of the following is not a functional interface in Java 8?',
-  //     questionType: 'Radio',
-  //     options: ['Consumer', 'Supplier', 'Runnable', 'Comparator'],
-  //     skills: 'Java-8',
-  //     Difficulty_Level: 'E',
-  //     answer: ['Comparator'],
-  //   };
-  // }
+  selectAllQuestions(tab: any) {
+    if (tab && tab.content) {
+      tab.content.forEach((question: any) => {
+        question.selection = true;
+      });
+      this.selectedQuestion = tab.content;
+    }
+  }
+  unselectAllQuestions(tab: any) {
+    if (tab && tab.content) {
+      tab.content.forEach((question: any) => {
+        question.selection = false;
+      });
+      this.selectedQuestion = [];
+    }
+  }
 }
