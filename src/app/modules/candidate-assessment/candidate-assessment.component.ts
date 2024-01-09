dbf63785bc23ca77461c7f461cd9bda28937e442
Aishumph <aishwarya.rajagopal@mphasis.com>
2024-01-09 12:03:58 +0530

    connected backend in candidate home page

--- src/app/modules/candidate-assessment/candidate-assessment.component.ts
+++ src/app/modules/candidate-assessment/candidate-assessment.component.ts
@@ -1,49 +1,84 @@
-import { AfterViewInit, Component, OnInit, } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
-
+import { CandidateAssessmentService } from 'src/app/services/candidate-assessment.service';
+import { ManagernameService } from 'src/app/services/managername.service';
+ 
 @Component({
   selector: 'app-candidate-assessment',
   templateUrl: './candidate-assessment.component.html',
   styleUrls: ['./candidate-assessment.component.scss'],
-  providers: [MessageService]
+  providers: [MessageService],
 })
-export class CandidateAssessmentComponent implements AfterViewInit{
-
+export class CandidateAssessmentComponent implements AfterViewInit {
   visible: boolean = false;
-  assessmentData: any=[{
-    testName: 'Angular Full Stack for Junior developers',
-    skills : ["AWS","Java"],
-    cutoff : 75,
-    duration: 10,
-    validity : "24-JAN"
-  },
-  {
-    testName: 'NodeJs Dev',
-    skills : ["Node.Js","Java"],
-    cutoff : 65,
-    duration: 15,
-    validity : "28-JAN"
-  }];
-  constructor(private messageService: MessageService) {}
+  candidateEmail!: string | null;
+  assessmentData: any;
+  // assessmentData: any = [
+  //   {
+  //     testName: 'Angular Full Stack for Junior developers',
+  //     skills: ['AWS', 'Java'],
+  //     cutoff: 75,
+  //     duration: 10,
+  //     validity: '24-JAN',
+  //   },
+  //   {
+  //     testName: 'NodeJs Dev',
+  //     skills: ['Node.Js', 'Java'],
+  //     cutoff: 65,
+  //     duration: 15,
+  //     validity: '28-JAN',
+  //   },
+  // ];
+  constructor(
+    private messageService: MessageService,
+    private managernameService: ManagernameService,
+    private candidateAssessmentService: CandidateAssessmentService,
+    private router: Router
+  ) {}
   ngAfterViewInit(): void {
-    console.log("onInit")
     this.show();
+    this.candidateEmail =
+      this.managernameService.getCandidateAssessment_Email();
+    console.log('Mail Id', this.candidateEmail);
+    // localStorage.setItem('Candidateemail', this.candidateEmail);
+    // this.candidateEmail = localStorage.getItem("Candidateemail");
+    this.getAssessmentdatabyEmail();
   }
  
-    show() {
-        this.messageService.add({ severity: 'info', detail: 'You have been assigned a new assessment on 23-Dec-2023' , sticky: true});
-    }
-
-    showAssessmentDialog(){
-      this.visible = true;
-    }
-
-    cancelButton(){
-      this.visible = false;
-    }
-
-    startAssessment(){
-      
-    }
-
+  getAssessmentdatabyEmail() {
+    this.candidateAssessmentService
+      .getCandidatedata_by_Email(this.candidateEmail)
+      .subscribe((response) => {
+        this.assessmentData = response;
+        console.log('candidate data', this.assessmentData);
+ 
+        //setting the data for assessment page
+        this.candidateAssessmentService.setAssessmentData(this.assessmentData);
+      });
+  }
+ 
+  show() {
+    this.messageService.add({
+      severity: 'info',
+      detail: 'You have been assigned a new assessment on 23-Dec-2023',
+      sticky: true,
+    });
+  }
+ 
+  showAssessmentDialog() {
+    this.visible = true;
+  }
+ 
+  cancelButton() {
+        this.visible = false;
+ 
+  }
+ 
+  startAssessment() {
+    this.router.navigate(['candidatequestion']);
+  }
+ 
+ 
 }
+ 
