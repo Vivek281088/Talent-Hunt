538b958490aec6910e70c01d26dd0bff8165bff6
ANANTH4004 <balamuruganananth@gmail.com>
2024-03-06 14:41:16 +0530

    skillset filter working

--- src/app/modules/schedulepage/schedulepage.component.ts
+++ src/app/modules/schedulepage/schedulepage.component.ts
@@ -9,7 +9,7 @@ import { CandidateAssessmentService } from 'src/app/services/candidate-assessmen
 import { ReviewerService } from 'src/app/services/reviewer.service';
 
 import { FormControl } from '@angular/forms';
-import { MenuItem } from 'primeng/api';
+import { FilterMetadata, MenuItem } from 'primeng/api';
 import { Table } from 'primeng/table';
 import { DataService } from 'src/app/services/data.service';
 import { NewScheduleService } from 'src/app/services/new-schedule.service';
@@ -114,6 +114,13 @@ export class SchedulepageComponent implements OnInit {
     this.getUniqueCandidatedata();
     this.getCandidatename();
   }
+  customFilter(value: any, filter: FilterMetadata): boolean {
+    const selectedSkills: string[] = filter ? filter.value : null;
+    if (selectedSkills && selectedSkills.length > 0) {
+        return selectedSkills.some(skill => value.Skill.includes(skill)); 
+    }
+    return true;
+}
   getFormattedSkills(skills: any): {
     skills: string[];
     remainingCount: number;
@@ -294,6 +301,7 @@ export class SchedulepageComponent implements OnInit {
      data.forEach((element: any) => {
          this.skillSet.push({ skill: element }); 
       });;
+    //this.skillSet = data
       console.log('Skill Set', data);
     });
   }
