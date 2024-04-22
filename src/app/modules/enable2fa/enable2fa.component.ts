f4d7cc2e5775ab4a6c168e54fddb4fd9af9e5fca
Aishwarya Rajagopal <aishwarya.rajagopal@mphasis.com>
2024-04-22 17:41:51 +0530

    Two Factor Auth design corrections

--- src/app/modules/enable2fa/enable2fa.component.ts
+++ src/app/modules/enable2fa/enable2fa.component.ts
@@ -4,7 +4,7 @@ import { CardModule } from 'primeng/card';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
-
+import { Clipboard } from '@angular/cdk/clipboard';
 @Component({
   selector: 'app-enable2fa',
   standalone: true,
@@ -15,11 +15,20 @@ import { ButtonModule } from 'primeng/button';
 export class Enable2faComponent {
 
   visible: boolean = false;
+  secretCode:string= 'bfhbfh34627865784';
   authCode = 'Aishu271200'; 
+
+
+  constructor(private clipboard: Clipboard) {}
+
   clickme(){
     this.visible=true;
   }
   cancelButton() {
     this.visible = false;
+  }
+
+  copyToClipboard(text: string) {
+    this.clipboard.copy(text);
   }
 }
