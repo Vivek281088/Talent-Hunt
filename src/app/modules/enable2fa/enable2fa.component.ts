baefcfce9bbc962bd2139ea7d3972c076eea44b1
Aishwarya Rajagopal <aishwarya.rajagopal@mphasis.com>
2024-04-23 18:07:45 +0530

    button design changes

--- src/app/modules/enable2fa/enable2fa.component.ts
+++ src/app/modules/enable2fa/enable2fa.component.ts
@@ -5,39 +5,78 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { Clipboard } from '@angular/cdk/clipboard';
+import { Subscription } from 'rxjs';
+import { DataService } from 'src/app/services/data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-enable2fa',
   standalone: true,
-  imports: [CommonModule, CardModule, ReactiveFormsModule, DialogModule, ButtonModule],
+  imports: [CommonModule, CardModule, ReactiveFormsModule, DialogModule, ButtonModule,FormsModule],
   templateUrl: './enable2fa.component.html',
   styleUrls: ['./enable2fa.component.scss'],
 })
 export class Enable2faComponent {
 
-  twofactvisible: boolean = false;
-  secretCode: string = 'bfhbfh34627865784';
-  authCode = 'Aishu271200';
-  copyMessage: string = 'Copied!'; 
+  twofactvisible: boolean = true;
   showTick: boolean= false;
+  subscription!: Subscription;
+  message!: any;
+  imageSource!:string;
+  secretKey!:string;
+  token!:string
 
-  constructor(private clipboard: Clipboard) {}
+  constructor(
+    private clipboard: Clipboard,
+    private router : Router,
+    private dataService:DataService
+  
+  
+  ) {}
 
-  clickme() {
-    this.twofactvisible = true;
+  ngOnInit() {
+    this.message = this.dataService.getSighupdata()
+    console.log("message received from signup",this.message);
+    this.createQrCode();
+    
   }
+
+  verifyNow()
+  {
+    const date= Date.now();
+    const id = new Date(date)
+    this.dataService.signupWithMFA(id,this.message.firstName,this.message.lastName,this.message.emailId,
+      this.message.phoneNumber,this.message.password,this.message.confirmPassword,this.token,this.secretKey).subscribe(
+        {
+          next : (response) => {
+            console.log(response)
+            this.router.navigate(['login'])
+          },
+          error : (error) => {
+            console.log(error)
+          }
+        }
+      )
+  }
+  createQrCode()
+  {
+    this.dataService.createQR(this.message.emailId).subscribe((response) => {
+      this.imageSource=response.imageSrc;
+      this.secretKey=response.secretKey
+      console.log('emailid created....',response);
+    });
+  }
+
 
   cancelButton() {
     this.twofactvisible = false;
   }
   copyToClipboard(text: string) {
     this.clipboard.copy(text);
-    this.copyMessage = 'Copied!';
     this.showTick = true; 
     console.log("showTick set to true"); 
         setTimeout(() => {
       this.showTick = false; 
-      this.copyMessage = ''; 
       console.log("showTick set to false");
     }, 1500); 
   }
