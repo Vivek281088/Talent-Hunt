521fd394bc924e779933cd17022b860ecd0264e8
Kabilanmph <kabilan.c@mphasis.com>
2024-04-26 12:57:55 +0530

    auth design changes

--- src/app/modules/enable2fa/enable2fa.component.ts
+++ src/app/modules/enable2fa/enable2fa.component.ts
@@ -58,12 +58,6 @@ export class Enable2faComponent {
             this.router.navigate(['login'])
           },
           error : (error) => {
-            this.messageservice.add({
-              severity: 'error',
-              summary: 'Wrong Authentication Code',
-              detail: ' Try Again!',
-              sticky: true
-            });
             this.showErrorMessage = true;
             console.log(error)
           }
