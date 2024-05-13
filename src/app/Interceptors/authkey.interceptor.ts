d4aba5151b86885de0b5eee26609ab6de42c05e3
ANANTH4004 <balamuruganananth@gmail.com>
2024-05-13 11:31:27 +0530

    .env file added to hide sensitive informations

--- src/app/Interceptors/authkey.interceptor.ts
+++ src/app/Interceptors/authkey.interceptor.ts
@@ -9,13 +9,13 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthkeyInterceptor implements HttpInterceptor {
-
+  API_KEY : string  = process.env.API_KEY as string;   
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const authReq = request.clone({
       headers: request.headers.set('Content-Type', 'application/json')
-          .set('x-api-key', '')
+          .set('x-api-key',this.API_KEY )
   });
     return next.handle(authReq);
   }
