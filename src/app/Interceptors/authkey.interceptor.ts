d222051fc47a0d88e7271b6cab02efc0a7d78e4c
ANANTH4004 <balamuruganananth@gmail.com>
2024-05-13 10:41:50 +0530

    removed api key

--- src/app/Interceptors/authkey.interceptor.ts
+++ src/app/Interceptors/authkey.interceptor.ts
@@ -15,7 +15,7 @@ export class AuthkeyInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const authReq = request.clone({
       headers: request.headers.set('Content-Type', 'application/json')
-          .set('x-api-key', 'KdUaeZD83v1xSSPQH8Ehy7qoUvmxp3Q82tdHYrZo')
+          .set('x-api-key', '')
   });
     return next.handle(authReq);
   }
