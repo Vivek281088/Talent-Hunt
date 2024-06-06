2b5243c89b23d6b39564e47719a390c49fe051f9
Baranimph <baranidharan.s@mphasis.com>
2024-06-06 17:47:08 +0530

    form changes

--- src/app/modules/manage-pcr/manage-pcr.component.ts
+++ src/app/modules/manage-pcr/manage-pcr.component.ts
@@ -44,8 +44,7 @@ export class ManagePcrComponent {
         [
           Validators.required,
           Validators.minLength(6),
-          Validators.maxLength(10),
-          this.validateId,
+          Validators.maxLength(10)
         ],
       ],
       pcrId: [
@@ -53,8 +52,7 @@ export class ManagePcrComponent {
         [
           Validators.required,
           Validators.minLength(6),
-          Validators.maxLength(10),
-          this.validateId,
+          Validators.maxLength(10)
         ],
       ],
       jobTitle: ['', [Validators.required, Validators.minLength(3)]],
@@ -64,8 +62,7 @@ export class ManagePcrComponent {
         [
           Validators.required,
           Validators.minLength(6),
-          Validators.maxLength(10),
-          this.validateId,
+          Validators.maxLength(10)
         ],
       ],
       skills: ['', [Validators.required]],
@@ -76,18 +73,18 @@ export class ManagePcrComponent {
     });
     this.pcr$ = this.store.select(getPcr);
   }
-  validateId(control: { value: any }) {
-    const value = control.value;
-    if (
-      !(
-        (typeof value === 'string' || typeof value === 'number') &&
-        /^[a-zA-Z0-9]{6,10}$/.test(String(value))
-      )
-    ) {
-      return { invalidInput: true };
-    }
-    return false;
-  }
+  // validateId(control: { value: any }) {
+  //   const value = control.value;
+  //   if (
+  //     !(
+  //       (typeof value === 'string' || typeof value === 'number') &&
+  //       /^[a-zA-Z0-9]{6,10}$/.test(String(value))
+  //     )
+  //   ) {
+  //     return { invalidInput: true };
+  //   }
+  //   return null;
+  // }
 
   onKeyPress(event: KeyboardEvent) {
     const inputLength = (event.target as HTMLInputElement).value.length;
@@ -116,6 +113,7 @@ export class ManagePcrComponent {
   }
   addPcr() {
     this.addPCR = true;
+
   }
 
   clear(table: Table) {
@@ -127,6 +125,8 @@ export class ManagePcrComponent {
     this.formSubmitted = false;
     this.editPCR = false;
     this.addPCRForm.reset();
+    this.addPCRForm.markAsPristine();
+    this.addPCRForm.markAsUntouched();
   }
   saveButton() {
     this.formSubmitted = true;
@@ -139,7 +139,7 @@ export class ManagePcrComponent {
         pcrId: formdata.pcrId,
         agileId: formdata.agileId,
         createdBy: formdata.createdBy,
-        createdDate: formdata.createdDate.toLocaleDateString('en-US'),
+       createdDate: formdata.createdDate.toLocaleDateString(),
         jobTitle: formdata.jobTitle,
         location: formdata.location,
         pcrStatus: formdata?.pcrStatus,
@@ -151,6 +151,8 @@ export class ManagePcrComponent {
       this.store.dispatch(PcrActions.addPCR({ pcr }));
       this.addPCR = false;
     }
+  this.cancelButton();
+
   }
   editData(data: any) {
     this.editPCR = true;
@@ -163,7 +165,7 @@ export class ManagePcrComponent {
         agileId: data.agileId,
         pcrId: data.pcrId,
         jobTitle: data.jobTitle,
-        createdDate: data.createdDate,
+        createdDate: new Date(data.createdDate),
         projectId: data.projectId,
         skills: data.skills,
         pcrStatus: data.pcrStatus,
@@ -181,7 +183,7 @@ export class ManagePcrComponent {
       pcrId: formdata.pcrId,
       agileId: formdata.agileId,
       createdBy: formdata.createdBy,
-      createdDate: formdata.createdDate.toLocaleDateString('en-US'),
+      createdDate: formdata.createdDate.toLocaleDateString(),
       jobTitle: formdata.jobTitle,
       location: formdata.location,
       pcrStatus: formdata?.pcrStatus,
@@ -191,5 +193,7 @@ export class ManagePcrComponent {
     };
     this.store.dispatch(PcrActions.updatePCR({ pcr }));
     this.editPCR = false;
+
+  this.cancelButton();
   }
 }
