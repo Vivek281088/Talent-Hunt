ca308ed9ae61adfd45ecd6d944bbf04ef08421fc
ANANTH4004 <balamuruganananth@gmail.com>
2024-06-10 16:24:51 +0530

    mainscreen

--- src/app/modules/mainscreen/mainscreen.component.ts
+++ src/app/modules/mainscreen/mainscreen.component.ts
@@ -18,27 +18,25 @@ export class MainscreenComponent implements OnInit {
   globalSearchValue!: string;
   l1Screen!:any;
   position: string = 'center';
+  clonedProducts: any = {};
 
 
   selectedCurrentStatus!:string;
-  currentStatus:any = [{name:"L1",value:"L1"},{name:"L2",value:"L2"},{name:"Rejected at Screening",value:"Rejected at Screening"},{name:"Rejected at Test",value:"Rejected at Test"},{name:"Mapped",value:"Mapped"},{name:"Onboarding",value:"Onboarding"}]
+  currentStatus:any = [{name:"L1",value:"L1"},{name:"L2",value:"L2"},{name:"Rejected at Screening",value:"Rejected at Screening"},
+    {name:"Rejected at Test",value:"Rejected at Test"},{name:"Mapped",value:"Mapped"},{name:"Onboarding",value:"Onboarding"}]
   panelMembers:any=[{names:"Indhu", value :"Indhu"},{names:"Vairavan",value :"Vairavan"},{names:"Suresh",value:"Suresh"},{names:"Alamelu",value:"Alamelu"}]
-  interviewStatus:any=[{names:"Selected",value:"Selected"},{names:"Rejected",value:"Rejected"},{names:"Pending",value:"Pending"}];
+  interviewStatus:any=[{names:"Selected",value:"Selected"},{names:"Rejected",value:"Rejected"},{names:"Pending",value:"Pending"},];
 
-  constructor( private L1ScreenService: L1ScreenService,private confirmationService: ConfirmationService,
+  constructor(private L1ScreenService: L1ScreenService,
+    private confirmationService: ConfirmationService,
     private messageService: MessageService,
-  ){
-
-
-  }
+  ){}
 
 
 
 
 ngOnInit(): void {
   this.getl1ScreenDetails();
-
-  //this.changeStatus();
   this.items = [
     { label: 'Home', routerLink: '/login', icon: 'pi pi-home' },
      { label: 'L1SCREEN', routerLink: '/manage-managers' },
@@ -52,7 +50,6 @@ clear(table: Table) {
 }
 
 isDisabled(l1:any):boolean{
-  //console.log("from disabled",l1)
     if(l1.testStatus=="Rejected"||l1.testStatus=="Not Scheduled"){
       return true;
     }
@@ -62,37 +59,50 @@ isDisabled(l1:any):boolean{
     }
 }
 
-// updateCheck(){
-//   console.log("......................button",this.l1Screen)
-// }
-
-
-// changeStatus()
-// {
-//   for(let i=0;i<this.l1Screen.length;i++){
-//   if(this.l1Screen[i].screeningDate&& this.l1Screen[i].testStatus=="Selected")
-//     {
-//       this.l1Screen[i].L1.l1Status=this.currentStatus[0].name;
-//       console.log("sapna",this.l1Screen)
-//     }
-
-//     else if(this.l1Screen[i].screeningDate&&this.l1Screen[i].testStatus=="Not Scheduled"){
-//       this.l1Screen[i].L1.l1Status=this.currentStatus[2].name
-//     }
-//   }
-// }
-
 getl1ScreenDetails() {
+  // this.L1ScreenService.getL1Details().subscribe((data) => {
+  //   this.l1Screen= data.map((data: any) => {
+  //     if (data.L1 && data.L1.l1InterviewDate) {
+  //       data.L1.l1InterviewDate = new Date(data.L1.l1InterviewDate);
+  //     }
+  //     return data;
+  //   });
+  //   console.log('llllllllllllllllllllllllll', this.l1Screen);
+  // });
   this.L1ScreenService.getL1Details().subscribe((data) => {
-    this.l1Screen= data.map((candidate: any) => {
-      if (candidate.L1 && candidate.L1.l1InterviewDate) {
-        candidate.L1.l1InterviewDate = new Date(candidate.L1.l1InterviewDate);
+    this.l1Screen = data.map((item: any) => {
+      if (!item.L1) {
+        item.L1 = { l1Panel: [], l1InterviewDate: null, l1Status: '' };
       }
-      return candidate;
+      if (item.L1.l1InterviewDate) {
+        item.L1.l1InterviewDate = new Date(item.L1.l1InterviewDate);
+      }
+      // Initialize missing L2 properties
+      if (!item.L2) {
+        item.L2 = { l2Panel: [], l2InterviewDate: null, l2Status: '' };
+      }
+      if (item.L2.l2InterviewDate) {
+        item.L2.l2InterviewDate = new Date(item.L2.l2InterviewDate);
+      }
+      // Initialize current bindings
+      
+      this.updateCurrentBindings(item);
+      // Set initial values for currentPanel, currentInterviewDate, and currentLStatus if they're empty
+      if (!item.currentPanel) {
+        item.currentPanel = item.currentStatus === 'L1' ? item.L1.l1Panel : item.L2.l2Panel;
+      }
+      if (!item.currentInterviewDate) {
+        item.currentInterviewDate = item.currentStatus === 'L1' ? item.L1.l1InterviewDate : item.L2.l2InterviewDate;
+      }
+      if (!item.currentLStatus) {
+        item.currentLStatus = item.currentStatus === 'L1' ? item.L1.l1Status : item.L2.l2Status;
+      }
+      
+      return item;
     });
-
-    console.log('llllllllllllllllllllllllll', this.l1Screen);
+    console.log("l1 screennnnn..................", this.l1Screen)
   });
+  
 }
 
 
@@ -130,27 +140,59 @@ confirmPosition(l1: any) {
   });
 }
 addL1Details(l1:any){
-   const l1Details = {
-
-     pcrId:l1.pcrId,
-  candidateId:l1.candidateId,
-  s:l1.screeningDate,
-  testStatus:l1.testStatus,
-  L1:l1.L1,
-  currentStatus:l1.currentStatus,
-
-   }
+   let l1Details = {}
+  if(l1.currentStatus == "L1"){
+    const L1 = {
+      l1InterviewDate : l1.currentInterviewDate,
+      l1Panel : l1.currentPanel,
+      l1Status : l1.currentLStatus
+    }
+     l1Details = {
+      pcrId:l1.pcrId,
+      candidateId:l1.candidateId,
+      screeningDate:l1.screeningDate,
+      testStatus:l1.testStatus,
+      L1:L1 ,
+      L2 : l1.L2,
+      currentStatus:l1.currentStatus,
+    }
+  }else if(l1.currentStatus == "L2"){
+    const L2 = {
+      l2InterviewDate : l1.currentInterviewDate,
+      l2Panel : l1.currentPanel,
+      l2Status : l1.currentLStatus
+    }
+    l1Details = {
+      pcrId:l1.pcrId,
+      candidateId:l1.candidateId,
+      screeningDate:l1.screeningDate,
+      testStatus:l1.testStatus,
+      L1:l1.L1,
+      L2 : L2,
+      currentStatus:l1.currentStatus,
+    }
+  }else{
+    l1Details = {
+      pcrId:l1.pcrId,
+      candidateId:l1.candidateId,
+      screeningDate:l1.screeningDate,
+      testStatus:l1.testStatus,
+      L1:l1.L1,
+      L2 : l1.L2,
+      currentStatus:l1.currentStatus,
+    }
+  }
+ 
    this.L1ScreenService.updateL1Details(l1Details).subscribe((data:any)=>{
     console.log("dataaaaaaaaaaaaaaaaaaaaaaaaa",data)
-
    })
 }
 onRowEditInit(product: any) {
-
+  this.clonedProducts[product.pcrId as string] = { ...product };
 }
 
 onRowEditSave(product: any) {
-  console.log(product)
+  console.log("lioasdkfnakjf", this.l1Screen)
   product.L1.l1InterviewDate =  product.L1.l1InterviewDate.toLocaleDateString()
 
 }
@@ -158,5 +200,23 @@ onRowEditSave(product: any) {
 onRowEditCancel(product: any, index: number) {
 
 }
-
+updateCurrentBindings(l1: any) {
+  if (l1.currentStatus === 'L1') {
+    l1.currentPanel = l1.L1.l1Panel;
+    l1.currentInterviewDate = l1.L1.l1InterviewDate;
+    l1.currentLStatus = l1.L1.l1Status;
+  } else if (l1.currentStatus === 'L2') {
+    l1.currentPanel = l1.L2.l2Panel;
+    l1.currentInterviewDate = l1.L2.l2InterviewDate;
+    l1.currentLStatus = l1.L2.l2Status;
+  }
+}
+onStatusChange(l1: any) {
+  // if (l1.currentStatus === 'L1') {
+  //   l1.L2.l2Panel = [];
+  //   l1.L2.l2InterviewDate = null;
+  //   l1.L2.l2Status = '';
+  // }
+  this.updateCurrentBindings(l1);
+}
 }
