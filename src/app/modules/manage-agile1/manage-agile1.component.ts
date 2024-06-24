4622fb46de1753d90edc76c0afa51c118f5d7310
ANANTH4004 <balamuruganananth@gmail.com>
2024-06-24 23:36:53 +0530

    pcr upload,agile upload and extract PCR details done

--- src/app/modules/manage-agile1/manage-agile1.component.ts
+++ src/app/modules/manage-agile1/manage-agile1.component.ts
@@ -2,26 +2,89 @@ import { Component } from '@angular/core';
 import { agileActions } from 'src/app/store/Agile1/Agile1.action';
 import { getAgile } from 'src/app/store/Agile1/Agile1.selector';
 import { Store } from '@ngrx/store';
+import * as XLSX from 'xlsx';
+import { PcrService } from 'src/app/services/pcr.service';
 
 @Component({
   selector: 'app-manage-agile1',
   templateUrl: './manage-agile1.component.html',
-  styleUrls: ['./manage-agile1.component.scss']
+  styleUrls: ['./manage-agile1.component.scss'],
 })
-export class ManageAgile1Component  {
-  agileData:any
-  constructor(private store:Store){
-
+export class ManageAgile1Component {
+  agileData: any;
+  constructor(private store: Store, private pcrService: PcrService) {}
+  ngOnInit() {
+    this.getAgileData();
   }
- ngOnInit(){
-  this. getAgileData();
- }
 
- getAgileData() {
-  this.store.dispatch(agileActions.getAgileDetails());
-  this.store.select(getAgile).subscribe((data) => {
-    console.log('Agile Details', data);
-    this.agileData = data;
-  });
-}
+  getAgileData() {
+    this.store.dispatch(agileActions.getAgileDetails());
+    this.store.select(getAgile).subscribe((data) => {
+      console.log('Agile Details', data);
+      this.agileData = data;
+    });
+  }
+  agileUpload(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      const reader: FileReader = new FileReader();
+      const tempdata = reader.readAsBinaryString(file);
+      console.log('temp data', tempdata);
+      reader.onload = (event) => {
+        console.log(event);
+        let binaryData = event.target?.result;
+        let workbook = XLSX.read(binaryData, { type: 'binary' });
+        console.log(workbook);
+        workbook.SheetNames.forEach((sheet) => {
+          const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+          const value = this.agileFormatData(data);
+          if (value.length > 0) {
+            const tempValue = value.map((temp: any) => ({
+              deleted: false,
+              ...temp,
+            }));
+            console.log(tempValue);
+            this.pcrService.addAgile1Details(tempValue).subscribe((data) => {
+              console.log('data fromt he backend ', data);
+            });
+          }
+        });
+      };
+    }
+  }
+  agileFormatData(data: any) {
+    let lastEntryWithId: any;
+    return data.reduce(
+      (
+        result: any[],
+        item: {
+          ID: string;
+          Job_Status: any;
+          Job_Vendor_Submitted: any;
+          Name: any;
+          Numubers: any;
+        }
+      ) => {
+        if (item.ID) {
+          const entry = {
+            ...item,
+            ID: item.ID.toString(),
+            employeeDetails: [],
+          };
+          result.push(entry);
+          lastEntryWithId = entry;
+        } else if (lastEntryWithId && item.ID === '') {
+          const additionalDetail = {
+            Job_Status: item.Job_Status,
+            Job_Vendor_Submitted: item.Job_Vendor_Submitted,
+            Name: item.Name,
+            Numubers: item.Numubers,
+          };
+          lastEntryWithId.employeeDetails.push(additionalDetail);
+        }
+        return result;
+      },
+      []
+    );
+  }
 }
